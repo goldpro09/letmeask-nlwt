010e98bcbc2fc144ba02bc573b06f79d00285267
Isaias <isaiashsobral@gmail.com>
2021-09-28 17:31:25 -0300

    uploading repository

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+import './services/firebase'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <h1>asdiojaio</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
